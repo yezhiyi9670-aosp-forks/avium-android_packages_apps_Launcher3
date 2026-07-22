b581aa14a93c7e40e624af2a7dad990e33ccfce8
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-07-22 14:09:38 +0800

    Launcher3: Remove Uninstall option in task overlay menu
    This shouldn't be a high-frequency entry for uninstallation, and
    poses risk of data loss due to misoperation.

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -125,7 +125,7 @@ public class TaskOverlayFactory {
             TaskShortcutFactory.APP_INFO,
             TaskShortcutFactory.KILL_APP,
             TaskShortcutFactory.SPLIT_SELECT,
-            TaskShortcutFactory.UNINSTALL,
+            // TaskShortcutFactory.UNINSTALL,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
